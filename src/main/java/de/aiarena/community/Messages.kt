5fa71ed4c416cde20ebb91a3dfa0b6022bd909d7
Monika Hackenberg <flameout@ai-arena.de>
2021-01-06 11:48:51 +0100

    FIXED - Missing message delimiter

--- src/main/java/de/aiarena/community/Messages.kt
+++ src/main/java/de/aiarena/community/Messages.kt
@@ -7,7 +7,7 @@ data class MessageFromServer(val code: Int, val headers: HashMap<String,String> 
 }
 data class MessageToServer(val cmd: String, val protocolVersion: String, val headers: HashMap<String,String> = HashMap()){
     override fun toString(): String {
-        return "$protocolVersion $cmd\n" + headers.entries.map{it.key+": "+it.value}.joinToString("\n")
+        return "$protocolVersion $cmd\n" + headers.entries.map{it.key+": "+it.value}.joinToString("\n")+"\n\n"
     }
     fun debugLog(){
         val lines = toString().split("\n")
